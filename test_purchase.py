3b6afc5339dbfe2ee125f1cec9a622d27cb786c0
Vld-NN <vnosov1@gmail.com>
2024-09-14 22:37:41 +0300

    modified file test_purchase.py

--- test_purchase.py
+++ test_purchase.py
@@ -16,6 +16,22 @@ try:
     driver.find_element(By.ID, 'login-button').click()
 
     # 2 Выбор товара
-    driver.find_element(By.XPATH, '//div[text()='Sauce Labs Backpack']').click()
-    driver.find_element(By.XPATH, '//button[text()='Add to cart']').click()
+    driver.find_element(By.XPATH, "//div[text()='Sauce Labs Backpack']").click()
+    driver.find_element(By.XPATH, "//button[text()='Add to cart']").click()
+
+    # 3 оформление покупки
+    driver.find_element(By.CLASS_NAME, "shoping_cart_link").click()
+    driver.find_element(By.XPATH, "//button[text()='Checkout']").click()
+
+    # Заполнение формы
+    driver.find_element(By.ID, "first-name").send_keys("John")
+    driver.find_element(By.ID, "last-name").send_keys("Doe")
+    driver.find_element(By.ID, "postal-code").send_keys("12345")
+    driver.find_element(By.XPATH, "//input[@value='Continue']").click()
+
+    # Завершение покупки
+    driver.find_element(By.XPATH, "//button[text()='Finish']").click()
+
+    # 4 Проверки успешной покупки/Завершение
+    success_message = driver.find_element(By.CLASS_NAME, "complet-header").text
 
